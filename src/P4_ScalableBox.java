62fad2e86d9752eca3d2adb76031907cfad8322d
Dave <37315071+DRAPP1@users.noreply.github.com>
2018-03-29 20:46:34 -0500

    t

--- src/P4_ScalableBox.java
+++ src/P4_ScalableBox.java
@@ -1,3 +1,4 @@
+
 /*
 	ISYS 320
 	Name(s):
@@ -9,36 +10,50 @@ import java.util.Scanner;
 public class P4_ScalableBox {
 	Scanner input = new Scanner(System.in);
 
-
 	public static void main(String[] args) {
+
+		String str, output;
+		int times;
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("HOW TALL? ");
+
+		str = input.nextLine();
+
+		System.out.println("HOW WIDE? ");
+
+		times = input.nextInt();
+
 		drawBoxCap(10);
 		drawBoxInsides(10);
 		drawBoxCap(10);
+
 	}
-	
-	public static void drawBoxCap(int i ) {
+
+	public static void drawBoxCap(int i) {
 		System.out.print("+");
-		
-		for( int capDashIndex = 1; capDashIndex <= 8 - 2; capDashIndex++ ) {
+
+		for (int capDashIndex = 1; capDashIndex <= 8 - 2; capDashIndex++) {
 			System.out.print("-");
 		}
-		
+
 		System.out.println("+");
 	}
-	
-	public static void drawBoxInsides(int i  ) {
-		for( int insideRowIndex = 1; insideRowIndex <= 10 - 2; insideRowIndex++ ) {
+
+	public static void drawBoxInsides(int i) {
+		for (int insideRowIndex = 1; insideRowIndex <= 10 - 2; insideRowIndex++) {
 			drawBoxInsideLine();
 		}
 	}
-	
-	public static void drawBoxInsideLine(  ) {
+
+	public static void drawBoxInsideLine() {
 		System.out.print("|");
-		
-		for( int insideRowIndex = 1; insideRowIndex <= 8 - 2; insideRowIndex++ ) {
+
+		for (int insideRowIndex = 1; insideRowIndex <= 8 - 2; insideRowIndex++) {
 			System.out.print(".");
 		}
-		
+
 		System.out.println("|");
 	}
 
