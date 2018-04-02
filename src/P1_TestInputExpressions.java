a1d4937d8d777c9293d5d84f697d219fee22b8db
Dave <37315071+DRAPP1@users.noreply.github.com>
2018-04-01 20:48:49 -0500

    h

--- src/P1_TestInputExpressions.java
+++ src/P1_TestInputExpressions.java
@@ -5,25 +5,27 @@
 
  // Your expected results after the input is provided:
  
- // 1.)
- // 2.)
- // 3.) 
- // 4.)
+ // 1.) nothing the code doesn't return anything
+ // 2.) nothing the code doesn't return anything
+ // 3.) nothing the code doesn't return anything
+ // 4.) An error since it doesn't take in strings
  
- // 5.)
- // 6.)
- // 7.)
- // 8.)
- 
+ // 5.) An error since it doesn't return a computation
+ // 6.) An error since it doesn't return a computation 
+ // 7.) An error since it doesn't take in strings 
+ // 8.) nothing the code doesn't return anything
+
 import java.util.Scanner;
 
 public class P1_TestInputExpressions {
 
 	public static void main(String[] args) {
-		Scanner console = new Scanner(System.in); 
-		System.out.print("How much money do you have? "); 
-		double money = console.nextDouble(); 
 		
+		System.out.print("How much money do you have? "); 
+		Scanner console = new Scanner(System.in); 
+		
+		double money = console.nextDouble(); 
+		 System.out.println(money*2);
 		
 
 	}
